ec1635c13d372d68f42d4262cbf781eca128f1ba
msejak <7o.mseJak@gmail.com>
2024-03-13 17:02:26 +0300

    feat: 30-react-redux Added book id

--- 30-react-redux/frontend/src/components/BookForm/BookForm.js
+++ 30-react-redux/frontend/src/components/BookForm/BookForm.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
 import { addBook } from '../../redux/books/actionCreators';
 
 import './BookForm.css';
@@ -16,11 +17,10 @@ const BookForm = () => {
       const book = {
         title,
         author,
+        id: uuidv4(),
       };
 
-      console.log(addBook(book));
       dispatch(addBook(book));
-
       setTitle('');
       setAuthor('');
     }
