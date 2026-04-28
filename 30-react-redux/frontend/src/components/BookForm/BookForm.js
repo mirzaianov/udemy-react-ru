3ee06966922d0c08629025de752dc12bbedecfa8
Riaz Mirzaianov <87698216+mirzaianov@users.noreply.github.com>
2026-04-28 11:36:42 +0300

    feat: 30-react-redux 368

--- 30-react-redux/frontend/src/components/BookForm/BookForm.js
+++ 30-react-redux/frontend/src/components/BookForm/BookForm.js
@@ -32,7 +32,7 @@ const BookForm = () => {
   };
 
   const handleAddRandomBookViaAPI = async () => {
-    dispatch(fetchBook());
+    dispatch(fetchBook('http://localhost:4000/random-book'));
   };
 
   return (
