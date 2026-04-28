9e4b56f3a56a52f185c2c772940f7d604e62b2b9
Riaz Mirzaianov <87698216+mirzaianov@users.noreply.github.com>
2026-04-28 10:01:39 +0300

    feat: 30-react-redux 359

--- 30-react-redux/frontend/src/components/BookForm/BookForm.js
+++ 30-react-redux/frontend/src/components/BookForm/BookForm.js
@@ -16,14 +16,14 @@ const BookForm = () => {
     const randomIndex = Math.floor(Math.random() * booksData.length);
     const randomBook = booksData[randomIndex];
 
-    dispatch(addBook(createBookWithID(randomBook)));
+    dispatch(addBook(createBookWithID(randomBook, 'random')));
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
     if (title && author) {
-      dispatch(addBook(createBookWithID({ title, author })));
+      dispatch(addBook(createBookWithID({ title, author }, 'manual')));
       setTitle('');
       setAuthor('');
     }
@@ -34,7 +34,7 @@ const BookForm = () => {
       const res = await axios.get('http://localhost:4000/random-book');
 
       if (res?.data?.title && res?.data?.author) {
-        dispatch(addBook(createBookWithID(res.data)));
+        dispatch(addBook(createBookWithID(res.data, 'api')));
       }
     } catch (error) {
       console.error('Error fetching random book:', error);
